7cdd9984c209a1aa9c9d29e495fa9a616d257aa5
haythem-said <haythem.said97@gmail.com>
2023-04-23 22:16:29 +0100

    second commit

---
+++ src/components/Section.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+const Section = () => {
+  return (
+    <Wrap>
+        <ItemText>
+            <h1>Model S</h1>
+            <p>Order Online for Touchless Delivery</p>
+        </ItemText>
+    </Wrap>
+  )
+}
+
+export default Section
+const Wrap=styled.div`
+width:100vw;
+height:100vh;
+background-size:cover;
+background-position:center;
+background-image:url('/images/model-x.jpg')
+`
+const ItemText=styled.div`
+padding-top:15vh;
+text-align:center
+`
